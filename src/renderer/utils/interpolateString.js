666da110c674d000d5c0bb0e4cf5b012f1bda608
Fortyseven <fortyseven@gmail.com>
2018-08-22 04:58:52 -0400

    Added support for `$TIME` variable; formatting

--- src/renderer/utils/interpolateString.js
+++ src/renderer/utils/interpolateString.js
@@ -1,10 +1,21 @@
-function  hashify( title ) {
+function hashify(title) {
     let stripped = title.trim().toLowerCase();
     stripped = stripped.replace(/\W/g, "");
     return `#${stripped}`;
 };
 
-export default function( $state, instring ) {
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = hours + ':' + minutes + ':' + date.getSeconds() +' ' + ampm;
+    return strTime;
+}
+
+export default function ($state, instring) {
     // Mapping values are either a string referencing the
     // store getter name, or a function returning a value.
     const MAPPINGS = {
@@ -13,16 +24,19 @@ export default function( $state, instring ) {
         TWITCH_USER: "getTwitchUser",
         GAME_HASHTAG: () => {
             return hashify($state.getters['getStreamGame'])
+        },
+        TIME: () => {
+            return formatAMPM(new Date());
         }
     }
 
     // Iterate over mappings and replace with data from
     // the store.
 
-    for(let i in MAPPINGS ) {
+    for (let i in MAPPINGS) {
         let key = "$" + i;
 
-        if ( instring.indexOf( key ) > -1 ) {
+        if (instring.indexOf(key) > -1) {
             let getter_name = MAPPINGS[i];
             let value = "";
 
