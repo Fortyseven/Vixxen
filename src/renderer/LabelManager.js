ded8515aa7c823727c0907d586aea33d199eb8b7
Fortyseven <fortyseven@gmail.com>
2018-09-14 19:39:15 -0400

    Minor JS language tweaks

--- src/renderer/LabelManager.js
+++ src/renderer/LabelManager.js
@@ -1,5 +1,5 @@
 import EventBus from "./EventBus";
-var fs = require("fs");
+const fs = require("fs");
 
 var labelManager = {
     _store: null,
@@ -14,26 +14,23 @@ var labelManager = {
         this.disable(false);
         this._labels = [];
 
-        var label_entries = this._store.getters.getLabels;
+        let label_entries = this._store.getters.getLabels;
 
-        for (var i in label_entries) {
+        for (let i in label_entries) {
             this._labels[i] = {
                 entry: label_entries[i],
                 timer: null
             };
         }
 
-        // If we didn't start off paused, then resume
-        // the service
+        // If we didn't start off paused, then resume the service
         if (was_running) {
             this.enable(false);
         }
-
-        console.info("reloaded labels", this._labels);
     },
     /******************************/
     enable(savestate = true) {
-        for (var i in this._labels) {
+        for (let i in this._labels) {
             this._labels[i].timer = setInterval(
                 function() {
                     this._processLabel(this._labels[i].entry);
@@ -41,14 +38,16 @@ var labelManager = {
                 this._labels[i].entry.interval
             );
         }
+
         this._is_running = true;
+
         if (savestate) {
             this._vue.$store.commit("setLabelState", true);
         }
     },
     /******************************/
     disable(savestate = true) {
-        for (var i in this._labels) {
+        for (let i in this._labels) {
             clearInterval(this._labels[i].timer);
         }
         this._is_running = false;
